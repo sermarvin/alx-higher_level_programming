8ad60b31e6597089468182f9ccb9103395bf9900
sermarvin <marv.kariuki@gmail.com>
2023-09-08 17:18:11 +0300

    copy string and remove character at position n

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    new_string = ""
+    for char in range(len(str)):
+        if char == n:
+            char += 1
+        new_string += str[char]
+    return (new_string)
